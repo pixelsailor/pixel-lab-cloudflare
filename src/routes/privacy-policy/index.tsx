5047f38545e2fbca9daf7c68f998d466c88d5659
pixelsailor <smoov.b@gmail.com>
2025-01-21 10:58:03 -0600

    remove unused dependency

--- src/routes/privacy-policy/index.tsx
+++ src/routes/privacy-policy/index.tsx
@@ -1,5 +1,5 @@
 import { component$ } from "@builder.io/qwik";
-import { routeLoader$, type RequestHandler } from "@builder.io/qwik-city";
+import { routeLoader$ } from "@builder.io/qwik-city";
 
 type WpRenderedObject = {
   rendered: string;
