3f51cb1eba0c675cb6fdc771bb6fa77cdcf01ed6
pixelsailor <smoov.b@gmail.com>
2025-01-21 09:35:06 -0600

    add tailwind and privacy-policy

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,36 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { Link, type DocumentHead, type RequestHandler } from "@builder.io/qwik-city";
+
+export const onGetMedia: RequestHandler = async ({ send, url }) => {
+  const response = await fetch(
+    new URL('https://cms.pixel-lab.us/wp-json/wp/v2/media/', url)
+  );
+  send(response.status, await response.text());
+}
 
 export default component$(() => {
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
+    <div class="container h-screen grid md:grid-cols-2 gap-4 place-items-center">
+      <div class="grid gap-4 text-center">
+        <h1 class="silkscreen text-6xl font-light leading-normal m-0" style="color: #333;">Pixel Lab</h1>
+        <p class="text-sm mb-0">Powered by Cloudflare Pages and <a href="https://qwik.dev">qwik</a></p>
+        <div>
+          <Link href="/privacy-policy">Privacy Policy</Link>
+        </div>
       </div>
-    </>
+      <div>
+        <img width="720" height="540" src="https://cms.pixel-lab.us/wp-content/uploads/2025/01/3048031.F4ECB10BF007F006.webp" alt="A gray mech-like model created with LEGO bricks" />
+      </div>
+    </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "pixel lab",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "Pixel Lab is a sandbox for experimenting with various web technologies in a live environment. Pixel Lab is strictly for educational purposes.",
     },
   ],
 };
---
+++ src/routes/privacy-policy/index.tsx
@@ -0,0 +1,34 @@
+import { component$ } from "@builder.io/qwik";
+import { routeLoader$, type RequestHandler } from "@builder.io/qwik-city";
+
+type WpRenderedObject = {
+  rendered: string;
+}
+
+export const usePrivacyPolicy = routeLoader$(async () => {
+  const res = await fetch('https://cms.pixel-lab.us/wp-json/wp/v2/pages/3');
+  const page = await res.json();
+  return page as {
+    date: string | null;
+    guid: any;
+    id: number;
+    link: string;
+    slug: string;
+    status: string;
+    type: string;
+    permalink_template: string;
+    title: WpRenderedObject;
+    content: WpRenderedObject;
+    author: number;
+  }
+});
+
+export default component$(() => {
+  const signal = usePrivacyPolicy();
+  return (
+    <div class="md:container mx-auto p-4">
+      <h1 class="text-4xl font-bold mb-8">{signal.value.title.rendered}</h1>
+      <div dangerouslySetInnerHTML={signal.value.content.rendered}></div>
+    </div>
+  )
+});
